35deb33baf3e41957fffb147c0ca1bda8eb19f3a
Sephy <nobody@noone.void>
2016-10-18 17:13:49 +0200

    Refactoring code & Adding comments
    - Refactoring code in order to be more convenient to the ESGI specifications.
    - Adding comments (JavaDoc) to gestbib.h

--- gestbib.c
+++ gestbib.c
@@ -10,35 +10,29 @@
 #include <stdlib.h>
 #include "./gestbib.h"
 
-void createDictionnary(char* filename, char** listOfWords, int sizeOfListOfWords)
-{
+void createDictionnary(char* filename, char** listOfWords, int sizeOfListOfWords) {
 	FILE* f = fopen(filename, "w");//on doit choisir une extension pour le dictionnaire
 
 	int i;
-	for(i = 0; i < sizeOfListOfWords; i++)
-	{
+	for(i = 0; i < sizeOfListOfWords; i++) {
 		fprintf(f, "%s\n",listOfWords[i] );
 	}
 	fclose(f);
 }
 
-void useExistingDictionnary(char* filename)
-{
+void useExistingDictionnary(char* filename) {
 	FILE* f = fopen(filename, "r");
 
-	if(f != NULL)
-	{
+	if(f != NULL) {
 		//je ne comprend pas exactement ce qu'il veux dire par le fait d'utiliser.
 	}
 	fclose(f);
 }
 
-void createDictionnaryFromTxt(char* fileTxtName)
-{
+void createDictionnaryFromTxt(char* fileTxtName) {
 	FILE* f = fopen(filename, "r");
 
-	if(f != NULL)
-	{
+	if(f != NULL) {
 		char** allUniqWordFromTxt; //récupérer tous les mots unique du dictionnaire
 		int sizeOfallUniqWordFromTxt;
 		char* newDictionnaryTitle = "";// setter un nom par rapport au dictionnaire?
@@ -47,54 +41,46 @@ void createDictionnaryFromTxt(char* fileTxtName)
 	fclose(f);
 }
 
-void destroyDictionnary(char* filename)
-{
+void destroyDictionnary(char* filename) {
 	File* f = fopen(filename, "w+");
-	if(f != NULL)
-	{
+	if(f != NULL) {
 		fputs(f, "");//delete le tout normalement;
 	}
 	fclose(f);
 }
 
-void insertWordIntoDictionnary(char* filename, char* word)
-{
+void insertWordIntoDictionnary(char* filename, char* word) {
 	FILE* f = fopen(filename, "a");
-	if(f != NULL)
-	{
+	if(f != NULL) {
 		fprintf(f, "%s\n", word);
 	}
 	fclose(f);
 }
 
-int searchWord(char* filename, char* word)
-{
+int searchWord(char* filename, char* word) {
 	FILE* f = fopen(filename, "r");
 	int isItInDictionnary = 0;
 
-	if(f != NULL)
-	{
+	if(f != NULL) {
 		fseek(f, 0, SEEK_END);
 		int sizeFile = ftell(f);
 		rewind(f);
-		do
-		{
+		do {
 			char* thisWordFromDictionnary;
 			fscanf("%s\n", thisWordFromDictionnary);
-			if(strcmp(thisWordFromDictionnary, word) == 0)
-			{
+			if(strcmp(thisWordFromDictionnary, word) == 0) {
 				isItInDictionnary = 1;
 				break;
 			}
-		}
-		while(ftell(f) < sizeFile);
+		} while(ftell(f) < sizeFile);
 
 	}
 
-	if(isItInDictionnary == 1)
+	if(isItInDictionnary == 1) {
 		printf("%s est bien dans le dictionnaire",word);
-	else
+	} else {
 		printf("%s n'est pas dans le dictionnaire\n", word);
+	}
 
 	fclose(f);
 	return isItInDictionnary;
--- gestbib.h
+++ gestbib.h
@@ -1,6 +1,35 @@
+/**
+ * Function which aime to create a dictionary.
+ * @param filename The file which will be created
+ * @param listOfWords The list of words which the dictionary representated by its @param filename will contain.
+ * @param sizeOfListOfWords The size of the given @param listOfWords
+ */
 void createDictionnary(char* filename, char** listOfWords, int sizeOfListOfWords);
+/**
+ * Fuction which aim to use a dictionary representated by its @param filename
+ * @param filename The file name of the dictionary.
+ */
 void useExistingDictionnary(char* filename);
+/**
+ * Function which aim to create a dictionary from a text file
+ * @param fileTxtName The text file which will be used to create a dictionary
+ */
 void createDictionnaryFromTxt(char* fileTxtName);
+/**
+ * Function wich aim to destroy the dictionary representated by its @param filename
+ * @param filename The filename which represent the dictionary
+ */
 void destroyDictionnary(char* filename);
+/**
+ * Function which aim to insert a word into an existing dictionary representated by its @param filename
+ * @param filename The filename which represent the dictionary
+ * @param word The word to insert
+ */
 void insertWordIntoDictionnary(char* filename, char* word);
+/**
+ * Function which aim to search a word into a dictionary representated by its @param filename
+ * @param filename The filename which represent the dictionary
+ * @param word The word to search
+ * @return 0 if the word doesn't exist, 1 if the word exist
+ */
 int searchWord(char* filename, char* word);
